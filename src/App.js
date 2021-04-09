b024a2f40c671e592790e6edb4e72a1c367034f5
calmighted <calmighted@gmail.com>
2021-04-09 21:55:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css'
+import arrow from '../src/assests/images/icon-arrow.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.container}>
+        <div className={classes.backgroundImg}>
+          <h1 className={classes.Heading}>IP Address Tracker</h1>
+            <div className={classes.searchBox}>
+              <input type="text" placeholder="Search for any IP address or domain"/>
+                <div className={classes.arrowContainer}>
+                <img src={arrow} alt="enter address"/>
+                </div>
+            </div>
+          </div>
+          <div className={classes.infoBox}>
+            <div className={classes.infoContainer}>
+              <div className={classes.infoChild}>
+                <span className={classes.infoHead}>IP ADDRESS</span>
+                <span className={classes.infoData}>192.212.174.101</span>
+              </div>
+              <div className={classes.infoChild}>
+                <span className={classes.infoHead}>LOCATION</span>
+                <span className={classes.infoData}>Brooklyn, Ny 10001</span>
+              </div>
+              <div className={classes.infoChild}>
+                <span className={classes.infoHead}>TIMEZONE</span>
+                <span className={classes.infoData}>UTC -05:00</span>
+              </div>
+              <div className={classes.infoChild}>
+                <span className={classes.infoHead}>ISP</span>
+                <span className={classes.infoData}>SpaceX Starlink</span>
+              </div>
+            </div>
+          </div>
+          <div className={classes.map}>
+            Map
+          </div>
+      </div>
     </div>
   );
 }
